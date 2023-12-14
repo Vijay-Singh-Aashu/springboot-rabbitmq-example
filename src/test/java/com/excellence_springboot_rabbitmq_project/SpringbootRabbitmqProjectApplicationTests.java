ea1dc7dfb2e94ddaefd3c3c42ff076fbe73c4938
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-14 18:32:12 +0530

    base-project-created

---
+++ src/test/java/com/excellence_springboot_rabbitmq_project/SpringbootRabbitmqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_rabbitmq_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
