315f6a9a0977f9a0c374bad31428550d56c0311d
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-15 18:52:00 +0530

    sending and receiving message by rabbitmq

--- src/main/java/com/excellence_springboot_rabbitmq_project/SpringbootRabbitmqProjectApplication.java
+++ src/main/java/com/excellence_springboot_rabbitmq_project/SpringbootRabbitmqProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootRabbitmqProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRabbitmqProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqProjectApplication.class, args);
+    }
 
 }
